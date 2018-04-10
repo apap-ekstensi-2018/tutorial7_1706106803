223bdca19c81ef59d82a2faf7616f8d1264958df
lidianatalia <lidiamanik@outlook.com>
2018-04-11 00:51:05 +0700

    [1] make service [2] implement service consumer for view and vieawll

--- Tutorial07Consumer/tutorial5_1706106803/src/main/java/com/example/ApapTutorial04Application.java
+++ Tutorial07Consumer/tutorial5_1706106803/src/main/java/com/example/ApapTutorial04Application.java
@@ -2,6 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApapTutorial04Application
@@ -10,5 +12,10 @@ public class ApapTutorial04Application
     public static void main (String[] args)
     {
         SpringApplication.run (ApapTutorial04Application.class, args);
+    }
+    
+    @Bean
+    public RestTemplate restTemplate() {
+    	return new RestTemplate();
     }
 }
